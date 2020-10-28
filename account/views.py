ed66e671b878dd0fd2ea316d9c3de686d42cdee4
aa-ag <aaron.aguerrevere@gmail.com>
2020-10-28 17:10:57 -0500

    validation password confirmation

--- account/views.py
+++ account/views.py
@@ -6,7 +6,11 @@ def signup(request):
     Renders view where users complete form to sign up
     '''
     if request.method == 'POST':
-        User.objects.create_user(request.POST['username'], password=request.POST['password'])
-        return render(request, 'account/signup.html')
+        if request.POST['password'] == request.POST['passwordconfirmation']:
+            User.objects.create_user(request.POST['username'], password=request.POST['password'])
+            return render(request, 'account/signup.html')
+        else:
+            context = {'error': 'Passwords didn\'t match. Please try again.'}
+            return render(request, 'account/signup.html', context)
     else:
         return render(request, 'account/signup.html')
