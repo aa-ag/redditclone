667e54450c9d25c1511221a3c4882668e2985704
aa-ag <aaron.aguerrevere@gmail.com>
2020-10-28 17:03:25 -0500

    backend creating users

--- account/views.py
+++ account/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, HttpResponse
+from django.contrib.auth.models import User
 
 def signup(request):
+    '''
+    Renders view where users complete form to sign up
+    '''
     if request.method == 'POST':
-        return HttpResponse("working")
+        User.objects.create_user(request.POST['username'], password=request.POST['password'])
+        return render(request, 'account/signup.html')
     else:
         return render(request, 'account/signup.html')
