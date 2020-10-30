f226c08df8be3c7522e46f643d41a46d89e5f67d
aa-ag <aaron.aguerrevere@gmail.com>
2020-10-30 15:23:34 -0500

    added posted x time ago language in models

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+import math
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -8,4 +10,40 @@ class Post(models.Model):
     votes_total = models.IntegerField(default=0)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    # TO DO: convert pub_date to "X hours ago"
+    def __str__(self):
+        return self.title
+    
+    def ago(self):
+        now = timezone.now()
+
+        time_diff = now - self.pub_date
+
+        if time_diff.days == 0:
+            hours = math.floor(time_diff.seconds/3600)
+
+            if hours <= 1:
+                return "posted less than an hour ago"
+            else:
+                return f"posted {str(hours)} hours ago"
+        if time_diff.days >= 1 and time_diff.days < 30:
+            d = time_diff.days
+            if time_diff.d == 1:
+                return "posted yesterday"
+            else:
+                return f"posted {str(d)} days ago"
+
+        if time_diff.days >= 30 and time_diff.days < 365:
+            months = math.floor(time_diff.days/30)
+
+            if months == 1:
+                return "posted last month"
+            else:
+                return f"posted {months} months ago"
+
+        if time_diff.days >= 365:
+            years = math.floor(time_diff.days/365)
+
+            if years == 1:
+                return "posted last year"
+            else:
+                return f"posted {years} years ago"
