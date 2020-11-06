53704caed5bff2ded92c21c5ff981a93d24073a4
aa-ag <aaron.aguerrevere@gmail.com>
2020-11-06 16:41:32 -0600

    reverted home design to table

--- redditclone/urls.py
+++ redditclone/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from django.contrib import admin
 from posts import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('posts/', include('posts.urls')),
     path('', views.home, name="home"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
